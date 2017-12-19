0f4965ed93bcd1e1c5f42f19a0f19888cd666ff8
Matthew Roseman <mroseman95@gmail.com>
2017-12-18 16:36:50 -0800

    wrote a skeleton endpoint for fetching/adding media
    Signed-off-by: Matthew Roseman <mroseman95@gmail.com>

--- server.py
+++ server.py
@@ -1,7 +1,17 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
     return 'Hello World'
+
+
+@app.route('/media', methods=['GET', 'POST'])
+def add_media():
+    if request.method == 'GET':
+        # TODO grab all the media from the database and return it
+        pass
+    else:
+        media_name = request.form['name']
+        # TODO store this media in the database as unconsumed
