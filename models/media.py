8db25f293b4f759137c015caaf424822820ba576
Matthew Roseman <mroseman95@gmail.com>
2017-12-19 16:29:16 -0800

    added a repr function to User and Media models, and fixed a typo in Media model
    Signed-off-by: Matthew Roseman <mroseman95@gmail.com>

--- models/media.py
+++ models/media.py
@@ -1,8 +1,11 @@
 from gogomedia import db
 
 
-class Users(db.Model):
+class Media(db.Model):
     __tablename__ = 'media'
     id = db.Column('id', db.Integer, primary_key=True),
     medianame = db.Column('medianame', db.String(80), nullable=False),
     user = db.Column('user', db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    def __repr__(self):
+        return '<Media(id={}, medianame={}, user={})>'.format(self.id, self.medianame, self.user)
--- models/users.py
+++ models/users.py
@@ -1,7 +1,10 @@
 from gogomedia import db
 
 
-class Users(db.Model):
+class User(db.Model):
     __tablename__ = 'users'
     id = db.Column('id', db.Integer, primary_key=True)
     username = db.Column('username', db.String(50), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<User(id={}, username={})>'.format(self.id, self.username)
