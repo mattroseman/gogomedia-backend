f887a0627da75a26c01dc8bb63ccea6fbf2efd92
Matthew Roseman <mroseman95@gmail.com>
2018-02-17 14:45:08 -0500

    added test cases for adding, updating, and deleting models from the database
    Signed-off-by: Matthew Roseman <mroseman95@gmail.com>

--- tests.py
+++ tests.py
@@ -32,20 +32,82 @@ class GoGoMediaModelTestCase(GoGoMediaTestCase):
         db.session.commit()
 
         self.assertTrue(user in db.session)
+        self.assertEqual(user.id, 1)
+        self.assertEqual(user.username, 'testname')
+
+    def test_remove_user(self):
+        user = User('testname')
+        db.session.add(user)
+        db.session.commit()
+
+        self.assertTrue(user in db.session)
+
+        db.session.delete(user)
+        db.session.commit()
+
+        self.assertFalse(user in db.session)
 
     def test_add_media(self):
-        db.session.add(User('testname'))
+        user = User('testname')
+        db.session.add(user)
+        db.session.commit()
 
-        media = Media('testmedianame', 1)
+        media = Media('testmedianame', user.id)
+        db.session.add(media)
+        db.session.commit()
+
+        self.assertEqual(media.medianame, 'testmedianame')
+        self.assertEqual(media.user, user.id)
+        self.assertFalse(media.consumed)
+        self.assertTrue(media in db.session)
+
+    def test_add_consumed_media(self):
+        user = User('testname')
+        db.session.add(user)
+        db.session.commit()
+
+        media = Media('testmedianame', user.id, consumed=True)
+        db.session.add(media)
+        db.session.commit()
+
+        self.assertTrue(media.consumed)
+
+    def test_update_media(self):
+        user = User('testname')
+        db.session.add(user)
+        db.session.commit()
+
+        media = Media('testmedianame', user.id)
+        db.session.add(media)
+        db.session.commit()
+
+        self.assertFalse(media.consumed)
+
+        media.consumed = True
+        db.session.commit()
+
+        self.assertTrue(media.consumed)
+
+    def test_remove_media(self):
+        user = User('testname')
+        db.session.add(user)
+        db.session.commit()
+
+        media = Media('testmedianame', user.id)
         db.session.add(media)
         db.session.commit()
 
         self.assertTrue(media in db.session)
 
+        db.session.delete(media)
+        db.session.commit()
+
+        self.assertFalse(media in db.session)
+
 
 class GoGoMediaViewTestCase(GoGoMediaTestCase):
 
-    def test_index_view(self):
+    def test_index(self):
         response = self.client.get('/')
         self.assertEqual(response.status, '200 OK')
 
