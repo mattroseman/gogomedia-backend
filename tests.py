1ab37a7aa4588e68bed5fb7730eb574ecae42f1d
Matthew Roseman <mroseman95@gmail.com>
2018-02-17 16:57:37 -0500

    finished unit tests for all views
    Signed-off-by: Matthew Roseman <mroseman95@gmail.com>

--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 import unittest
 from flask_testing import TestCase
+import json
 
 from app import create_app
 from database import db
@@ -295,9 +296,196 @@ class GoGoMediaViewTestCase(GoGoMediaTestCase):
 
     def test_index(self):
         response = self.client.get('/')
-        self.assertEqual(response.status, '200 OK')
 
-        self.assertEqual(response.get_data(), b'Hello World')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.get_data(as_text=True), 'Hello World')
+
+    def test_user(self):
+        response = self.client.post('/user',
+                                    data=json.dumps({'username': 'testname'}),
+                                    content_type='application/json')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(json.loads(response.get_data(as_text=True)), {'success': True})
+
+        user = User.query.filter(User.username == 'testname').first()
+
+        self.assertIsNotNone(user)
+        self.assertEqual(user.username, 'testname')
+
+    def test_add_media(self):
+        user = User('testname')
+        db.session.add(user)
+        db.session.commit()
+
+        response = self.client.put('/user/testname/media',
+                                   data=json.dumps({'name': 'testmedianame'}),
+                                   content_type='application/json')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(json.loads(response.get_data(as_text=True)), {'success': True})
+
+        media = Media.query.filter((Media.user == user.id) & (Media.medianame == 'testmedianame')).first()
+
+        self.assertIsNotNone(media)
+        self.assertEqual(media.medianame, 'testmedianame')
+        self.assertEqual(media.user, user.id)
+        self.assertFalse(media.consumed)
+
+    def test_add_media_consumed(self):
+        user = User('testname')
+        db.session.add(user)
+        db.session.commit()
+
+        response = self.client.put('/user/testname/media',
+                                   data=json.dumps({'name': 'testmedianame', 'consumed': True}),
+                                   content_type='application/json')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(json.loads(response.get_data(as_text=True)), {'success': True})
+
+        media = Media.query.filter((Media.user == user.id) & (Media.medianame == 'testmedianame')).first()
+
+        self.assertIsNotNone(media)
+        self.assertEqual(media.medianame, 'testmedianame')
+        self.assertEqual(media.user, user.id)
+        self.assertTrue(media.consumed)
+
+    def test_add_media_unconsumed(self):
+        user = User('testname')
+        db.session.add(user)
+        db.session.commit()
+
+        response = self.client.put('/user/testname/media',
+                                   data=json.dumps({'name': 'testmedianame', 'consumed': False}),
+                                   content_type='application/json')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(json.loads(response.get_data(as_text=True)), {'success': True})
+
+        media = Media.query.filter((Media.user == user.id) & (Media.medianame == 'testmedianame')).first()
+
+        self.assertIsNotNone(media)
+        self.assertEqual(media.medianame, 'testmedianame')
+        self.assertEqual(media.user, user.id)
+        self.assertFalse(media.consumed)
+
+    def test_update_media(self):
+        user = User('testname')
+        db.session.add(user)
+        db.session.commit()
+
+        media = Media('testmedianame', user.id, consumed=False)
+        db.session.add(media)
+        db.session.commit()
+
+        response = self.client.put('/user/testname/media',
+                                   data=json.dumps({'name': 'testmedianame', 'consumed': True}),
+                                   content_type='application/json')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(json.loads(response.get_data(as_text=True)), {'success': True})
+
+        media_list = Media.query.filter((Media.user == user.id) & (Media.medianame == 'testmedianame')).all()
+
+        self.assertEqual(len(media_list), 1)
+        self.assertEqual(media_list, [media])
+        self.assertTrue(media_list[0].consumed)
+
+        response = self.client.put('/user/testname/media',
+                                   data=json.dumps({'name': 'testmedianame', 'consumed': False}),
+                                   content_type='application/json')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(json.loads(response.get_data(as_text=True)), {'success': True})
+
+        media_list = Media.query.filter((Media.user == user.id) & (Media.medianame == 'testmedianame')).all()
+
+        self.assertEqual(len(media_list), 1)
+        self.assertEqual(media_list, [media])
+        self.assertFalse(media_list[0].consumed)
+
+    def test_get_media(self):
+        user = User('testname')
+        db.session.add(user)
+        db.session.commit()
+
+        media1 = Media('testmedianame1', user.id)
+        media2 = Media('testmedianame2', user.id)
+        db.session.add(media1)
+        db.session.add(media2)
+        db.session.commit()
+
+        response = self.client.get('/user/testname/media')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(set(json.loads(response.get_data(as_text=True))), {'testmedianame1', 'testmedianame2'})
+
+    def test_get_consumed_media(self):
+        user = User('testname')
+        db.session.add(user)
+        db.session.commit()
+
+        media1 = Media('testmedianame1', user.id, consumed=True)
+        media2 = Media('testmedianame2', user.id, consumed=True)
+        media3 = Media('testmedianame3', user.id, consumed=False)
+        media4 = Media('testmedianame4', user.id, consumed=False)
+        media5 = Media('testmedianame5', user.id, consumed=True)
+        db.session.add(media1)
+        db.session.add(media2)
+        db.session.add(media3)
+        db.session.add(media4)
+        db.session.add(media5)
+        db.session.commit()
+
+        response = self.client.get('/user/testname/media?consumed=yes')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(set(json.loads(response.get_data(as_text=True))),
+                         {'testmedianame1', 'testmedianame2', 'testmedianame5'})
+
+    def test_get_unconsumed_media(self):
+        user = User('testname')
+        db.session.add(user)
+        db.session.commit()
+
+        media1 = Media('testmedianame1', user.id, consumed=True)
+        media2 = Media('testmedianame2', user.id, consumed=True)
+        media3 = Media('testmedianame3', user.id, consumed=False)
+        media4 = Media('testmedianame4', user.id, consumed=False)
+        media5 = Media('testmedianame5', user.id, consumed=True)
+        db.session.add(media1)
+        db.session.add(media2)
+        db.session.add(media3)
+        db.session.add(media4)
+        db.session.add(media5)
+        db.session.commit()
+
+        response = self.client.get('/user/testname/media?consumed=no')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(set(json.loads(response.get_data(as_text=True))),
+                         {'testmedianame3', 'testmedianame4'})
+
+    def test_delete_media(self):
+        user = User('testname')
+        db.session.add(user)
+        db.session.commit()
+
+        media = Media('testmedianame', user.id)
+        db.session.add(media)
+        db.session.commit()
+
+        response = self.client.delete('/user/testname/media',
+                                      data=json.dumps({'name': 'testmedianame'}),
+                                      content_type='application/json')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(json.loads(response.get_data(as_text=True)), {'success': True})
+
+        media_list = Media.query.filter(Media.user == user.id).all()
+
+        self.assertEqual(media_list, [])
 
 
 if __name__ == '__main__':
