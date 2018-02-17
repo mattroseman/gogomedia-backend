e14d8afecfdc3e6f8469cab15543fed30d758971
Matthew Roseman <mroseman95@gmail.com>
2018-02-16 19:53:49 -0500

    split tests into seperate classes for seperate areas of code. Models, Logic, and View test cases
    Signed-off-by: Matthew Roseman <mroseman95@gmail.com>

--- tests.py
+++ tests.py
@@ -5,6 +5,10 @@ from app import create_app
 from database import db
 
 from models.user import User
+from models.media import Media
+
+from logic.user import add_user
+from logic.media import add_media
 
 
 class GoGoMediaTestCase(TestCase):
@@ -19,11 +23,8 @@ class GoGoMediaTestCase(TestCase):
         db.session.remove()
         db.drop_all()
 
-    def test_index(self):
-        response = self.client.get('/')
-        self.assertEqual(response.status, '200 OK')
 
-        self.assertEqual(response.get_data(), b'Hello World')
+class GoGoMediaModelTestCase(GoGoMediaTestCase):
 
     def test_add_user(self):
         user = User('testname')
@@ -31,6 +32,24 @@ class GoGoMediaTestCase(TestCase):
         db.session.commit()
 
         self.assertTrue(user in db.session)
+
+    def test_add_media(self):
+        db.session.add(User('testname'))
+
+        media = Media('testmedianame', 1)
+        db.session.add(media)
+        db.session.commit()
+
+        self.assertTrue(media in db.session)
+
+
+class GoGoMediaViewTestCase(GoGoMediaTestCase):
+
+    def test_index_view(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status, '200 OK')
+
+        self.assertEqual(response.get_data(), b'Hello World')
 
 
 if __name__ == '__main__':
